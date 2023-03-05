cc73008e5ded6d5d46d67dd0b0203e0979ae0d94
Herman <smurovnemez@mail.ru>
2023-03-05 08:52:17 -0600

    feat: func arity

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import DomElement from "./task1";
-
-new DomElement()
+function foo(x,y,z) {
+    // ..
+  }
+  
+  var a = 3;
+  
+  foo( a, a * 2, a + a );
+  
+  
+  console.log(foo.length)            // 3 арность // проверка на арность функцию foo.length; 
---
+++ main.js
@@ -0,0 +1,10 @@
+function foo(x,y,z) {
+  // ..
+}
+
+var a = 3;
+
+foo( a, a * 2, a + a );
+
+
+console.log(foo.length)            // 3 арность // проверка на арность функцию foo.length; 
