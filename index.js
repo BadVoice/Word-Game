5205610886e3ea70e5374a0b333cd96fcf05a5dd
Herman <smurovnemez@mail.ru>
2023-03-08 11:14:32 -0600

    feat: basic functional game

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-function foo(x,y,z) {
-    // ..
-  }
-  
-  var a = 3;
-  
-  foo( a, a * 2, a + a );
-  
-  
-  console.log(foo.length)            // 3 арность // проверка на арность функцию foo.length; 
+let field = document.getElementById('field');
+let message = document.getElementById('message');
+let addBtn = document.getElementById('add')
+
+// создать массив в которые будут записываться города 
+const cities = [];
+
+console.log(cities)
+console.log(field.value)
+
+// получить значение с инпута
+function getInputValue(field) {
+    return field.value
+}
+
+function clearInput(field) {
+    return field.value = "";
+}
+
+// function clearArray() {
+//     cities.splice(0,cities.length);
+// }
+
+
+function validateValue() {
+    cities.forEach((city) => {
+        if (city.slice(-1) === field.value[0]) {
+            cities.push(field.value)
+
+        } else if (field.value === city) {
+            console.log(`такое слово уже есть ${city}`)
+            cities.pop(city)
+        }
+    })
+}
+
+function pushInputValue() {
+
+    if (cities.length === 0) {
+        cities.push(getInputValue(field))
+    } else {
+        validateValue()
+    }
+
+
+    console.log(cities)
+
+
+    clearInput(field)
+}
+
+addBtn.addEventListener("click", pushInputValue)
